478d2d82a0e13223c159119da053d960ff2a053c
Fabio Fumarola <fabiofumarola@gmail.com>
2018-02-25 16:58:29 +0100

    update

---
+++ test/it/gov/daf/DatasetServiceSpec.scala
@@ -0,0 +1,8 @@
+package it.gov.daf
+
+import org.scalatest.mockito.MockitoSugar
+import org.scalatest.{AsyncFlatSpec, Matchers}
+
+class DatasetServiceSpec extends AsyncFlatSpec with Matchers with MockitoSugar {
+
+}
